af51946b2819857b787d04d1af82c99caf58985d
JsssCode <gurupsdmail@gmail.com>
2020-05-13 13:09:48 +0300

    initCount fixed

--- src/app/core/store/product/product.effects.ts
+++ src/app/core/store/product/product.effects.ts
@@ -1,13 +1,12 @@
-import {Injectable} from '@angular/core';
-import {Actions, createEffect, ofType} from '@ngrx/effects';
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
 
-import {EMPTY} from 'rxjs';
-import {map, mergeMap, catchError} from 'rxjs/operators';
-import {productListLoad, productListLoadedSuccess, initProductCount} from './product.actions';
-import {products} from '../../fakeBackend/products';
-import {ProductService} from '../../services/product.service';
-import {Action} from '@ngrx/store';
-import {ProductModel} from '../../models/Product';
+import { EMPTY } from 'rxjs';
+import { map, mergeMap, catchError, tap } from 'rxjs/operators';
+import { productListLoad, productListLoadedSuccess, initProductCount } from './product.actions';
+import { products } from '../../fakeBackend/products';
+import { ProductService } from '../../services/product.service';
+import * as ProductActions from './product.actions';
 
 @Injectable()
 export class ProductEffects {
@@ -16,20 +15,21 @@ export class ProductEffects {
     ofType('[Product List] Load'),
     mergeMap(() => this.productService.getAll()
       .pipe(
-        map(productList => productListLoadedSuccess({productList})),
+        tap(v => console.log(v)),
+        map(productList => ProductActions.productListLoadedSuccess({ productList })),
         // map(productList => productListLoad({productList}))
       ))
-    )
+  )
   );
 
   productCount$ = createEffect(() => this.actions$.pipe(
-    ofType('[Product] Init Count'),
-    mergeMap(() => this.productService.initCount(products[0]) // сюда по идее надо передать id продукта, чтоб я смогла извлечь
-                                                                              // сам продукт,но как этот id правильно передать сюда?
+    ofType(ProductActions.initProductCount),
+    mergeMap((action) => this.productService.initCount(action.product) // сюда по идее надо передать id продукта, чтоб я смогла извлечь
+      // сам продукт,но как этот id правильно передать сюда?
       .pipe(
-        map(productCount => initProductCount({productCount})),
+        map(productCount => ProductActions.initProductCountSuccess({ productId: action.product.id, count: productCount })),
       ))
-    )
+  )
   );
 
   constructor(
