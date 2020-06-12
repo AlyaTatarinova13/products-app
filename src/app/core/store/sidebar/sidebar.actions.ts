60c6b2d79a4e8b0671f97513de5848d0a741b77c
Alina Tatarinova <foxfire13579@gmail.com>
2020-06-12 16:03:21 +0300

    Added store partly

---
+++ src/app/core/store/sidebar/sidebar.actions.ts
@@ -0,0 +1,16 @@
+import {createAction, props} from '@ngrx/store';
+import {SidebarItem} from '../../models/SidebarItems';
+
+export const sidebarLoad = createAction(
+  '[SidebarItems] Load'
+);
+
+export const sidebarLoadSuccess = createAction(
+  '[SidebarItems] Load Success',
+  props<{sidebarItems: SidebarItem}>()
+);
+
+export const sidebarLoadFailed = createAction(
+  '[SidebarItems] Load Failed',
+  props<{error: string}>()
+);
