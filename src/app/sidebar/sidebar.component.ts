1599755057bde7a23b67c41bf62faa1dc378217b
Alina Tatarinova <foxfire13579@gmail.com>
2020-07-07 13:48:33 +0300

    Fixed unsubscribe sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,13 +1,13 @@
-import {Component, Input, OnInit, ViewChild} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
 
 import {sidebarLoad} from '../core/store/sidebar/sidebar.actions';
 import {SidebarState} from '../core/store/sidebar/sidebar.reducers';
 import {SidebarItem} from '../core/models/SidebarItems';
 import {SidebarItemsService} from '../core/services/sidebar-items.service';
 import {selectSidebarItems} from '../core/store/sidebar/sidebar.selector';
-import {Observable} from 'rxjs';
+import {Observable, ReplaySubject} from 'rxjs';
 import {Store, select} from '@ngrx/store';
-import {map, tap} from 'rxjs/operators';
+import {map, takeUntil, tap} from 'rxjs/operators';
 import {initProductCount} from '../core/store/product/product.actions';
 import {RootState} from '../core/store';
 
@@ -16,10 +16,11 @@ import {RootState} from '../core/store';
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, OnDestroy {
   public items$: Observable<SidebarItem>;
   public items: SidebarItem | string;
   opened: boolean;
+  destroy: ReplaySubject<any> = new ReplaySubject<any>(1);
 
   constructor(private store: Store<RootState>) {
     // this.items$ = this.store.pipe(select(selectSidebarItems), tap(val => console.log('from app component', val)));
@@ -30,11 +31,15 @@ export class SidebarComponent implements OnInit {
 
   ngOnInit(): void {
     this.store.dispatch(sidebarLoad());
-    this.items$.subscribe(values => {
+    this.items$.pipe(takeUntil(this.destroy)).subscribe(values => {
       this.items = values;
     });
-
-
     // this.store.dispatch({ type: '[SidebarItems] Load' });
+  }
+
+
+  ngOnDestroy(): void {
+    this.destroy.next(null);
+    this.destroy.complete();
   }
 }
